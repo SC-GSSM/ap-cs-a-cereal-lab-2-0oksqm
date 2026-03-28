173ecf318feac9576be9b814b218e464bd162bf5
Samantha M <0oksqm@gmail.com>
2026-03-28 20:05:28 +0000

    modified:   src/main/java/CerealRunner.java

--- src/main/java/CerealRunner.java
+++ src/main/java/CerealRunner.java
@@ -60,9 +60,9 @@ public class CerealRunner
    *  @param: c - Cereal object
    */
     
-   public static double findNetCarbs(Cereal c)
+   public static double findNetCarbsPerCup(Cereal c)
    {
-      return (c.getCarbs() - c.getFiber());
+      return (c.getCarbs() - c.getFiber()) / c.getCups();
    }
   
 
@@ -132,7 +132,7 @@ public class CerealRunner
       System.out.println("\n*****Find Net Carbs Results*****");
       Cereal testCereal = new Cereal("Golden Crisp",100,0,11,0.88);
       System.out.println("Expected results: 11.0");
-      System.out.println("Actual results:   " + findNetCarbs(testCereal));
+      System.out.println("Actual results:   " + findNetCarbsPerCup(testCereal));
 
       for(Cereal c: cereals) { 
       if(c.getName().equals("All-Bran with Extra Fiber") ||   
